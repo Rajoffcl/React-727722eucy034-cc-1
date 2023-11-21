314912d7a99b8b6bc0ad3702ec9ddcee63e57b62
Rajoffcl <134670720+Rajoffcl@users.noreply.github.com>
2023-11-21 16:15:16 +0530

    Add files via upload
    Qn no- 2

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { useState } from 'react';
+
+const ShowHideText = () => {
+  const [isTextVisible, setIsTextVisible] = useState(true);
+
+  const toggleTextVisibility = () => {
+    setIsTextVisible(!isTextVisible);
+  };
+
+  return (
+    <div className='box'>
+      <button onClick={toggleTextVisibility}>
+        {isTextVisible ? 'Hide Text' : 'Show Text'}
+      </button>
+
+      {isTextVisible && <p>This is the text that can be shown or hidden.</p>}
+    </div>
+  );
+};
+
+export default ShowHideText;
